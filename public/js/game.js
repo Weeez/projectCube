26a19f7f3194802d7c40efa0b881fd97a828bf04
Balázs András <xeropax@gmail.com>
2017-03-06 16:20:49 +0000

    heroku rotation

--- public/js/game.js
+++ public/js/game.js
@@ -158,7 +158,6 @@ function shaderWtf(){
         -0.5, 0.0, 0.0,
         0.5, 0.0, 0.0,
         0.0, 1.0, 0.0
-        
     ]);
     
     geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));

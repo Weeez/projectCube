e63efe4abaf5a4545d44c1563b41ab3168496f93
Balázs András <xeropax@gmail.com>
2017-03-14 15:01:29 +0000

    heroku waka te ki wo kurel

--- public/js/game.js
+++ public/js/game.js
@@ -51,7 +51,6 @@ document.addEventListener('keydown', function(e){
     keyPressed[e.keyCode] = true;
 }, false);
 
-
 document.addEventListener('keyup', function(e){
     keyPressed[e.keyCode] = false;
 }, false);
